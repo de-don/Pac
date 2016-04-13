bfeea5f7bf7a2082bc71d38fa641ada0e3599b36
Денис <de-don@mail.ru>
2016-04-13 12:34:19 +0300

    Файл для генерации случайных вершин

---
+++ Generate.py
@@ -0,0 +1,14 @@
+# Генерация случайного графа, для дайнейшей обработки
+import random
+
+N = 20  # Число вершин
+M = 50  # Число ребер
+#  Размеры поля
+h = 600
+w = 600
+
+print(N)
+for i in Names:
+    x = random.randint(0,w)
+    y = random.randint(0,h)
+    print(i, x, y)
