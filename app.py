6dc3f2c4c6579272ebfdbd28c6d6b4566a615324
qart17 <kubr1704@yandex.ru>
2016-04-15 06:11:24 +0700

    q

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from main import City
 
 
 class Example(QtGui.QWidget):
-
-    scale = 0
+    scale_w = 0
+    scale_h = 0
     max_vert_h = 1
     max_vert_w = 1
     city = None
@@ -17,7 +17,6 @@ class Example(QtGui.QWidget):
         self.max_vert_h = self.city.max_heigth()
         self.max_vert_w = self.city.max_width()
         self.set_scale()
-        print(self.scale)
 
     def initUI(self):
         self.showMaximized()
@@ -42,27 +41,40 @@ class Example(QtGui.QWidget):
         pen1 = QtGui.QPen()
         pen1.setWidth(2)
         pen1.setColor(QtCore.Qt.blue)
+        pen2 = QtGui.QPen()
+        pen2.setWidth(2)
+        pen2.setColor(QtCore.Qt.magenta)
         qp.setPen(pen0)
 
         for k in self.city.Vertex:
-            qp.drawPoint(self.city.Vertex[k][0] * self.scale, self.city.Vertex[k][1] * self.scale)
-            qp.drawText(self.city.Vertex[k][0] * self.scale - 10, self.city.Vertex[k][1] * self.scale - 10, k)
+            qp.drawPoint(self.city.Vertex[k][0] * self.scale_w, self.city.Vertex[k][1] * self.scale_h)
+            qp.drawText(self.city.Vertex[k][0] * self.scale_w - 10, self.city.Vertex[k][1] * self.scale_h - 10, k)
         qp.setPen(pen1)
+
+        way = self.city.GoTo(pFrom='A', pTo='B', Way=[])
         for k, v in self.city.City.items():
             x1, y1 = self.city.Vertex[k]
             for k2, d in v.items():
                 x2, y2 = self.city.Vertex[k2]
-                qp.drawLine(x1 * self.scale, y1 * self.scale, x2 * self.scale, y2 * self.scale)
-                qp.drawText((x1 + x2) * self.scale / 2 + 10, (y1 + y2) * self.scale / 2 + 10, str(d))
+                print(way)
+                if k in way and k != way[-1] and way[way.index(k) + 1] == k2:
+                    qp.setPen(pen2)
+                elif k2 in way and k2 != way[-1] and way[way.index(k2) + 1] == k:
+                    qp.setPen(pen2)
+                else:
+                    qp.setPen(pen1)
+                qp.drawLine(x1 * self.scale_w, y1 * self.scale_h, x2 * self.scale_w, y2 * self.scale_h)
+                qp.drawText((x1 + x2) * self.scale_w / 2 + 10, (y1 + y2) * self.scale_h / 2 + 10, str(d))
 
     def set_scale(self):
-        ratio = self.frameGeometry().width() / self.frameGeometry().height()
-        self.scale = int(0.8 * (self.frameGeometry().height()
-                             if self.max_vert_h > self.max_vert_w and self.max_vert_w / self.max_vert_h < ratio
-                             else
-                             self.frameGeometry().width())
-                           / max(self.max_vert_w, self.max_vert_h)
-                      )
+        self.scale_w = 0.8 * self.frameGeometry().width() / self.max_vert_w
+        self.scale_h = 0.8 * self.frameGeometry().height() / self.max_vert_h
+        # self.scale = int(0.8 * (self.frameGeometry().height()
+        #                      if self.max_vert_h > self.max_vert_w and self.max_vert_w / self.max_vert_h < ratio
+        #                      else
+        #                      self.frameGeometry().width())
+        #                    / max(self.max_vert_w, self.max_vert_h)
+        #               )
 
 
 def main():
